53269ae5b5100ea9547bd6d8f02ba4eba54dbdf5
chenyuting <chenyuting@tuniu.com>
2020-05-09 23:45:46 +0800

    koa-demo

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
-// 导入koa，和koa 1.x不同，在koa2中，我们导入的是一个class，因此用大写的Koa表示:
 const Koa = require('koa');
 
-// 注意require('koa-router')返回的是函数:
-const router = require('koa-router')();
+const bodyParser = require('koa-bodyparser');
 
-// 创建一个Koa对象表示web app本身:
+const controller = require('./controller');
+const createEnv = require('./nunjucks');
+const env = createEnv('views', {
+    watch: true,
+    filters: {
+        hex: function (n) {
+            return '0x' + n.toString(16);
+        }
+    }
+});
 const app = new Koa();
 
+var s = env.render('hello.html', {
+    name: '<Nunjucks>',
+    fruits: ['Apple', 'Pear', 'Banana'],
+    count: 12000
+});
+
+console.log(s);
+
+console.log(env.render('extend.html', {
+    header: 'Hello',
+    body: 'bla bla bla...'
+}));
+
+// log request URL:
 app.use(async (ctx, next) => {
-    console.log(`Process ${ctx.request.method} ${ctx.request.url}`); // 打印URL
-    await next(); // 调用下一个middleware
-});
-
-// add url-route:
-router.get('/hello/:name', async (ctx, next) => {
-    var name = ctx.params.name;
-    ctx.response.body = `<h1>Hello, ${name}!</h1>`;
-});
-
-router.get('/', async (ctx, next) => {
-    ctx.response.body = '<h1>Index</h1>';
-});
-
-// add router middleware:
-app.use(router.routes());
-
-app.use(async (ctx, next) => {
-    const start = new Date().getTime(); // 当前时间
-    await next(); // 调用下一个middleware
-    const ms = new Date().getTime() - start; // 耗费时间
-    console.log(`Time: ${ms}ms`); // 打印耗费时间
-});
-
-app.use(async (ctx, next) => {
+    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
     await next();
-    ctx.response.type = 'text/html';
-    ctx.response.body = '<h1>Hello, koa2!</h1>';
 });
 
-// 在端口3000监听:
+// parse request body:
+app.use(bodyParser());
+
+// add controllers:
+app.use(controller());
+
 app.listen(3000);
 console.log('app started at port 3000...');
